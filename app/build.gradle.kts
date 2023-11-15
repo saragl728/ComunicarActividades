86d90f57ddb02066eb2b1cd7f57f6b91ff5f88c5
Usuario <saragl728@gmail.com>
2023-11-15 08:32:56 +0100

    Habilito el binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.comunicaractividades"
     compileSdk = 33
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.comunicaractividades"
         minSdk = 24
