29bb2c7bfd6ab15cd0f9d0e1e1fe5e240db7c0df
Usuario <saragl728@gmail.com>
2023-11-15 08:25:01 +0100

    Inicializo con dos actividades

---
+++ app/src/main/java/com/example/comunicaractividades/Pantalla2.kt
@@ -0,0 +1,11 @@
+package com.example.comunicaractividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantalla2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla2)
+    }
+}
