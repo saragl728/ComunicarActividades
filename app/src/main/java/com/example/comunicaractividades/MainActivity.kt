554741520c811aa7fa75dbe2379cdd120809b33c
Usuario <saragl728@gmail.com>
2023-11-15 08:46:47 +0100

    Uno las pantallas

--- app/src/main/java/com/example/comunicaractividades/MainActivity.kt
+++ app/src/main/java/com/example/comunicaractividades/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.comunicaractividades
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.comunicaractividades.databinding.ActivityMainBinding
@@ -10,5 +11,16 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //evento de hacer click sobre acceder
+        binding.botonAcceder.setOnClickListener {
+            var nombre = binding.nombre.text.toString()
+            //se crea el intent para comunicar con Pantalla2
+            var intent = Intent(this, Pantalla2::class.java)
+            intent.putExtra("Dato", nombre)
+
+            //se accede a la otra pantalla
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/comunicaractividades/Pantalla2.kt
+++ app/src/main/java/com/example/comunicaractividades/Pantalla2.kt
@@ -2,10 +2,16 @@ package com.example.comunicaractividades
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.comunicaractividades.databinding.ActivityPantalla2Binding
 
 class Pantalla2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pantalla2)
+        //setContentView(R.layout.activity_pantalla2)
+
+        val binding = ActivityPantalla2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
